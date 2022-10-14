53c7aa760828bae888a0fd04e740a915f346dd51
Don Dang <105334355+dondangp@users.noreply.github.com>
2022-10-14 15:15:40 -0500

    Time: 58 ms (31.86%), Space: 13.8 MB (95.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        #set two variables
-        one,two = 1,1
-        #iterating through n-1
+        
+        one, two = 1,1
         for i in range (n-1):
-            #making new variable
             temp = one
             one = one + two
             two = temp
