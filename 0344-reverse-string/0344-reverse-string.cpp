d7103240c25a9fd56003147d5faafd9a0a851010
Don Dang <105334355+dondangp@users.noreply.github.com>
2022-10-13 21:30:06 -0500

    Time: 58 ms (20.93%), Space: 23 MB (93.48%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s);
+};
+/*********************************************************************/
+void Solution::reverseString(vector<char>& s) {
+    int size = s.size(), left, right;
+    char aux;
+    for (left = 0, right = size-1; left < right; ++left, --right) {
+        aux = s[left];
+        s[left] = s[right];
+        s[right] = aux;
+    }
+    return;
+}
+/*********************************************************************/
