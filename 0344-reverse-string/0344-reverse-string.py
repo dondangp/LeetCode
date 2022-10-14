c6321d055e8e61aad145cf0040749eaec68a0a75
Don Dang <105334355+dondangp@users.noreply.github.com>
2022-10-13 21:18:14 -0500

    Time: 427 ms (43.84%), Space: 18.3 MB (82.80%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,6 +3,4 @@ class Solution:
         wholeLength = len(s)
         for i in range(wholeLength//2):
             #swap
-            s[i],s[wholeLength-1-i] = s[wholeLength-1-i], s[i]
-        
-    
+            s[i], s[wholeLength-1-i] = s[wholeLength-1-i], s[i]
