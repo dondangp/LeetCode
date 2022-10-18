990d1a6d3ab409cce1713ee31e714f249f4e7f0c
Don Dang <105334355+dondangp@users.noreply.github.com>
2022-10-17 20:55:55 -0500

    Time: 57 ms (49.48%), Space: 13.9 MB (11.70%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -1,6 +1,11 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
+        #26 letters in alphabet
         if len(set(sentence)) == 26:
-            return 1
+            #return true
+            return 1 
+        #return false
         else:
             return 0
+        
+            
